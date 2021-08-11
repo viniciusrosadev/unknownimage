63422364aeaabee5205a6de39276c3dfd3c83375
Vinícius Rosa <viniciusrosa3525@gmail.com>
2021-08-10 23:25:04 -0300

    Picture update

--- src/pages/picture/[...unkownimage].tsx
+++ src/pages/picture/[...unkownimage].tsx
@@ -1,21 +1,52 @@
 import { Flex, Text, SimpleGrid, Box, HStack, Img } from '@chakra-ui/react'
 import { Search } from '../../components/search'
 import { ShareButton } from '../../components/shareButton'
+import { apiUnsplash } from '../../services/api'
 
-export default function UnkownImage() {
+interface IUnkownImage {
+    params: {
+        unkownimage: string
+    }
+}
+
+interface IImage {
+    image: PictureInformation
+}
+
+type PictureInformation = {
+    id: string;
+    created_at: string;
+    description: string;
+    alt_description: string;
+    width: number;
+    height: number;
+    location?: {
+        city: string;
+        country: string;
+    }
+    urls: {
+        full: string;
+        regular: string;
+    }
+    links: {
+        download: string;
+    }
+}
+
+export default function UnkownImage({ image }: IImage) {
     return (
         <>
             <Search />
 
             <Flex mt="4">
                 <Text color="red.500">
-                    Picture title
+                {image.description ?? 'Image from Unsplash'}
                 </Text>
             </Flex>
 
             <Flex align="baseline" justify="center">
                 <Box padding="1">
-                    <Img borderRadius="sm" src="https://comparaplano.com.br/wp-content/uploads/2019/09/dog-tv.png" />
+                    <Img borderRadius="sm" src={image.urls.regular} alt={image.alt_description}/>
                 </Box>
             </Flex>
 
@@ -27,7 +58,7 @@ export default function UnkownImage() {
 
                 <HStack>
                     <Text>
-                    Description
+                        {image.description ?? 'Image from Unsplash'}
                     </Text>
                 </HStack>
 
@@ -39,4 +70,41 @@ export default function UnkownImage() {
             </SimpleGrid>
         </>
     )
+}
+
+// The fallback generate new pages in build time
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: 'blocking',
+    }
+}
+
+export async function getStaticProps({ params }: IUnkownImage) {
+    const pictureId = params.unkownimage[0].slice(0,-2)
+
+    const picFromUnsplash = await apiUnsplash.get(`/photos/${pictureId}`).then(response => response.data)
+
+    const picture: PictureInformation = {
+        id: picFromUnsplash.id,
+        width: picFromUnsplash.width,
+        height: picFromUnsplash.height,
+        description: picFromUnsplash.description,
+        alt_description: picFromUnsplash.alt_description,
+        created_at: picFromUnsplash.created_at,
+        urls: {
+            full: picFromUnsplash.urls.full,
+            regular: picFromUnsplash.urls.regular
+        },
+        links: {
+            download: picFromUnsplash.links.download
+        }
+    }
+
+    return {
+        props: {
+            image: picture
+        },
+        revalidate: 86400,
+    }
 }
